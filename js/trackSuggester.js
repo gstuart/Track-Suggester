97b6b2e532da4f6090583486a639624f1a335758
Grace Stuart <gstuart@gmail.com>
2017-02-17 12:00:46 -0800

    changed var value from dropdown to radiobutton.

--- js/trackSuggester.js
+++ js/trackSuggester.js
@@ -7,14 +7,14 @@ $(document).ready(function() {
     $("#cssQuestion").show(); //this will display the first question.
 
     //declare varables for all the inputs of the questions, which will be used in the if/else statements below.
-    var cssResponse = $("#cssAnswer").val();
-    var cSharpResponse = $("cSharpAnswer").val();
-    var phpResponse = $("phpAnswer").val;
-    var java1Response = $("java1Answer").val();
-    var java2Response = $("java2Answer").val();
+    var cssResponse = $("input:radio[name=radioBtn]:checked").val;
+    var cSharpResponse = $("input:radio[name=radioBtn]:checked").val;
+    var phpResponse = $("input:radio[name=radioBtn]:checked").val;
+    var java1Response = $("input:radio[name=radioBtn]:checked").val;
+    var java2Response = $("input:radio[name=radioBtn]:checked").val;
 
     if ("cssResponse") { //if the first answer is yes, then replace the jumbotron with the text below.
-      $("#welcome").hide();
+    //  $("#welcome").hide();
       $("#result").show("You should join the CSS/Design course!");
     }
     else {

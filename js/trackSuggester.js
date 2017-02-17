103ccf28794079557be4b8faed4a95018b9e5248
Grace Stuart <gstuart@gmail.com>
2017-02-17 10:56:57 -0800

    create button submit funcion, and declared var for all question selections.

--- js/trackSuggester.js
+++ js/trackSuggester.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+  $("#beginBtn").click(function(){ //when the button is clicked do the following:
+    alert("this is a test");
+    #beginBtn.hide(); //this will hide the beginbtn
+    //declare varables for all the inputs of the questions, which will be used in the if/else statements below.
+    var cssResponse = $("#cssAnswer").val();
+    var cSharpResponse = $("cSharpAnswer").val();
+    var phpResponse = $("phpAnswer").val;
+    var java1Response = $("java1Answer").val();
+    var java2Response = $("java2Answer").val();
 
-  event.preventDefault();
+
+    event.preventDefault();
+  });
 });

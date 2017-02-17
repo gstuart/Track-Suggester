e343441bf12e873038ccb624b79f8339c1b0fa28
Grace Stuart <gstuart@gmail.com>
2017-02-17 11:01:38 -0800

    add else statement, and function to show first question.

--- js/trackSuggester.js
+++ js/trackSuggester.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
   $("#beginBtn").click(function(){ //when the button is clicked do the following:
     alert("this is a test");
     #beginBtn.hide(); //this will hide the beginbtn
+
+    #cssQuestion.show(); //this will display the first question.
+
     //declare varables for all the inputs of the questions, which will be used in the if/else statements below.
     var cssResponse = $("#cssAnswer").val();
     var cSharpResponse = $("cSharpAnswer").val();
@@ -9,6 +12,12 @@ $(document).ready(function() {
     var java1Response = $("java1Answer").val();
     var java2Response = $("java2Answer").val();
 
+    if (true) {
+
+    } else {
+      #welcome.hide();
+      #result.show("You should join the PHP course!");
+    }
 
     event.preventDefault();
   });

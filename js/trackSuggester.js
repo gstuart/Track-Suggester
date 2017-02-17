fd59dadbd66e3c496801526a2ae0712a09505631
Grace Stuart <gstuart@gmail.com>
2017-02-17 13:39:21 -0800

    correct error by correcting name and giving id to radio buttons.

--- js/trackSuggester.js
+++ js/trackSuggester.js
@@ -3,6 +3,17 @@ $(document).ready(function() {
     $("#beginBtn").hide(); //this will hide the beginbtn
     $("#cssQuestion").show(); //this will display the first question.
 
+    var yes = $("input[type='radio'][name=css][id=cssYes]:checked").val();
+    var no = $("input[type='radio'][name=css][id=cssNo]:checked").val();
+
+    $("yes").click(function(){
+      if (true) {
+        $("#welcome").hide();
+      }
+      else {
+        alert("They selected no");
+      }
+    });
     //declare varables for all the inputs of the questions, which will be used in the if/else statements below.
     // var cssResponse = $("input:radio[name=radioBtn]:checked").val;
     // var cSharpResponse = $("input:radio[name=radioBtn]:checked").val;

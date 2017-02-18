5e6e49182b1f59d99160225bf4f0c3b7097d12a9
Grace Stuart <gstuart@gmail.com>
2017-02-18 15:40:29 -0800

    add button to name of function.

--- js/trackSuggester.js
+++ js/trackSuggester.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   radioChoice = $("input:radio[id=radioBtn]:checked").val; // variable for all question selections, whether yes or no.
 
-  $("#beginBtn").click(function() { // when the button is clicked do the following:
-    $("#beginBtn").fadeOut(); //this will hide the beginbtn
+  $("button#beginBtn").click(function() { // when the button is clicked do the following:
+    $("button#beginBtn").fadeOut(); //this will hide the beginbtn
     $(".jumbotron").fadeOut(); //this will hide the jubmotron
     $("#form").fadeIn(); //this will display the form.
-    $("#submitBtn").fadeIn();
+    $("button#submitBtn").fadeIn();
 
     event.preventDefault();
   });

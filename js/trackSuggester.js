a412c19a6cbd60fe5e6443a9190a093628594d95
Grace Stuart <gstuart@gmail.com>
2017-02-19 09:22:58 -0800

    declare variables globally.

--- js/trackSuggester.js
+++ js/trackSuggester.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-  radioChoice = $("input:radio[class=radioBtn]:checked").val; // variable for all question selections, whether yes or no.
+  // business, or back-end, logic:
+  var javaInput = $("input:radio[value=java]:checked").val; // variable for all java selections
+  var rubyInput = $("input:radio[value=ruby]:checked").val; // variable for all ruby selections
+  var cSharpInput = $("input:radio[value=cSharp]:checked").val; //variable for all c# selections
+  var phpInput = $("input:radio[value=php]:checked").val; // variable for all php selections
+
+  // user interface, or front-end, logic:
   $("button#beginBtn").click(function() { // when the button is clicked, do the following:
     $("button#beginBtn").hide(); // hide the beginbtn
     $(".jumbotron").hide(); // hide the jubmotron
@@ -9,18 +15,17 @@ $(document).ready(function() {
       $("#questions").fadeOut(); // hide #questions
       $("button#submitBtn").hide(); // hide #submitBtn
       // function to determine which course user should take.
-      if (true) {
-        $("input:radio[id=java]:checked").val; // if in question1 the value "java" is selected then
+      if (javaInput) {
+       $("input:radio[id=java]:checked").val; // if in question1 the value "java" is selected then
         alert("java");
-  //      $(".jumbotron").hide(); // hide the jumbotron
         $("#result#java").show(); // display #javaDscrption within #result section
       } // closes if statement
-        else if (true) {
+        else if (rubyInput) {
           alert("esle if #1 - ruby");
           $("input:radio[id=ruby]:checked").val(); // else if question1 the value "ruby" is selected then
           $("#result#rubyDscrptn").show(); // display #rubyDscrptn within #result section
         } // closes first else if statement
-        else if (true) {
+        else if (cSharpInput) {
           alert("else if #2 - c#");
           $("input:radio[id=cSharp]:checked").val(); // else if question1 the value "cSharp" is selected then
           $("#result#cSharpDscrptn").show(); // display #cSharpDscrptn within #result section

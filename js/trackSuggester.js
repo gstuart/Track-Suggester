030d7cf7dd77aeed5e82c88c347ac7f2732b799e
Grace Stuart <gstuart@gmail.com>
2017-02-18 22:19:24 -0800

    add show function.

--- js/trackSuggester.js
+++ js/trackSuggester.js
@@ -3,28 +3,29 @@ $(document).ready(function() {
   $("button#beginBtn").click(function() { // when the button is clicked do the following:
     $("button#beginBtn").hide(); //this will hide the beginbtn
     $(".jumbotron").hide(); //this will hide the jubmotron
-    $("#questions").show(); //this will display the form.
+  //  $("#questions").show(); //this will display the form.
     $("button#submitBtn").show();
-    $("button#submitBtn").submit(function() {
-      $("#questions").fadeOut(); // hide #questions
-      $("button#submitBtn").hide();// hide #submitBtn
-      // function to determine which course user should take.
-        // if in question1 the value "java" is selected then
-          // display #welcome
-          // dispaly ("Based on your selections, you should go into the <bold><em>Java</bold></em> course. + #javaDscrptn")
-
-          // else if question1 the value "ruby" is selected then
-          // display #welcome
-          // dispaly "Based on your selections, you should go into the <bold><em>Ruby</bold></em> course. + #rubyDscrptn") in #welcome section
-
-          // else if question1 the value "cSharp" is selected then
-          // display #welcome
-          // dispaly ("Based on your selections, you should go into the <bold><em>C#</bold></em> course. + #cSharpDscrptn") in #welcome section
-
-        //else
-          // display #welcome
-          // display ("Based on your selections, you should go into the <bold><em>PHP</bold></em> course. + #phpDscrptn") in #welcome section
-    }); //closes submitbtn click
+    $("#result").show("Based on your selections, you should go into the <bold><em>Java</bold></em> course! + #javaDscrptn");
+    // $("button#submitBtn").submit(function() {
+    //   $("#questions").fadeOut(); // hide #questions
+    //   $("button#submitBtn").hide();// hide #submitBtn
+    //   // function to determine which course user should take.
+    //     // if in question1 the value "java" is selected then
+    //       $("#result").show("Based on your selections, you should go into the <bold><em>Java</bold></em> course! + #javaDscrptn"); // display #result
+    //       // display ("Based on your selections, you should go into the <bold><em>Java</bold></em> course. + #javaDscrptn")
+    //
+    //       // else if question1 the value "ruby" is selected then
+    //       // display #welcome
+    //       // display "Based on your selections, you should go into the <bold><em>Ruby</bold></em> course. + #rubyDscrptn") in #welcome section
+    //
+    //       // else if question1 the value "cSharp" is selected then
+    //       // display #result
+    //       // displayy ("Based on your selections, you should go into the <bold><em>C#</bold></em> course. + #cSharpDscrptn")
+    //
+    //     //else
+    //       // display #result
+    //       // display ("Based on your selections, you should go into the <bold><em>PHP</bold></em> course. + #phpDscrptn")
+    // }); //closes submitbtn click
 
     event.preventDefault();
   }); //closes beginbtn click

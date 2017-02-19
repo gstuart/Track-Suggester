cc3e0de29b240c2921470829112d898a3ae80b10
Grace Stuart <gstuart@gmail.com>
2017-02-18 19:05:02 -0800

    add commented out code explaining what needs to be placed in file, and add hide questions on submitbtn.click.

--- js/trackSuggester.js
+++ js/trackSuggester.js
@@ -4,9 +4,15 @@ $(document).ready(function() {
   $("button#beginBtn").click(function() { // when the button is clicked do the following:
     $("button#beginBtn").fadeOut(); //this will hide the beginbtn
     $(".jumbotron").fadeOut(); //this will hide the jubmotron
-    $("#questions").fadeIn(); //this will display the form.
+    $("#questions").show(); //this will display the form.
     $("button#submitBtn").fadeIn();
+    $("button#submitBtn").click(function() {
+      $("questions").fadeOut(); // hide #questions
+
+      // hide #submitBtn
+      // function to determine which course user should take.
+    }); //closes submitbtn click
 
     event.preventDefault();
-  });
-});
+  }); //closes beginbtn click
+}); //closes document.ready

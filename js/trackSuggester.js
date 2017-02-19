9617821d2aaefdec26124f8c61ba4b4e9e913cfa
Grace Stuart <gstuart@gmail.com>
2017-02-18 19:08:55 -0800

    correct error by adding hastag to questions function.

--- js/trackSuggester.js
+++ js/trackSuggester.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-  radioChoice = $("input:radio[id=radioBtn]:checked").val; // variable for all question selections, whether yes or no.
+  //radioChoice = $("input:radio[id=radioBtn]:checked").val; // variable for all question selections, whether yes or no.
 
   $("button#beginBtn").click(function() { // when the button is clicked do the following:
     $("button#beginBtn").fadeOut(); //this will hide the beginbtn
     $(".jumbotron").fadeOut(); //this will hide the jubmotron
     $("#questions").show(); //this will display the form.
     $("button#submitBtn").fadeIn();
-    $("button#submitBtn").click(function() {
-      $("questions").fadeOut(); // hide #questions
+    $("button#submitBtn").submit(function() {
+      $("#questions").fadeOut(); // hide #questions
 
       // hide #submitBtn
       // function to determine which course user should take.

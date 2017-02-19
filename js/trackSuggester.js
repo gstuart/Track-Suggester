b257fa88caefdb422db9219d7d1ed1a41a86643e
Grace Stuart <gstuart@gmail.com>
2017-02-18 16:05:39 -0800

    change name of div from form to questions.

--- js/trackSuggester.js
+++ js/trackSuggester.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   $("button#beginBtn").click(function() { // when the button is clicked do the following:
     $("button#beginBtn").fadeOut(); //this will hide the beginbtn
     $(".jumbotron").fadeOut(); //this will hide the jubmotron
-    $("#form").fadeIn(); //this will display the form.
+    $("#questions").fadeIn(); //this will display the form.
     $("button#submitBtn").fadeIn();
 
     event.preventDefault();

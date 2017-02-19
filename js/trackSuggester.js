ffc3d7b1537b423be5933023df3919a2183955fe
Grace Stuart <gstuart@gmail.com>
2017-02-19 08:44:24 -0800

    remove hide jumbotron, and change input ids to = value.

--- js/trackSuggester.js
+++ js/trackSuggester.js
@@ -7,25 +7,31 @@ $(document).ready(function() {
     $("button#submitBtn").show(); // show the form submit button
     $("button#submitBtn").click(function() { //when submit button is clicked, do the following:
       $("#questions").fadeOut(); // hide #questions
-      $("button#submitBtn").hide();// hide #submitBtn
+      $("button#submitBtn").hide(); // hide #submitBtn
       // function to determine which course user should take.
       if (true) {
-        $("input:radio[id=javaDscrptn]:checked").val; // if in question1 the value "java" is selected then
-        $("#result#javaDscrptn").show(); // display #javaDscrption within #result section
+        $("input:radio[id=java]:checked").val; // if in question1 the value "java" is selected then
+        alert("java");
+  //      $(".jumbotron").hide(); // hide the jumbotron
+        $("#result#java").show(); // display #javaDscrption within #result section
       } // closes if statement
         else if (true) {
-          $("input:radio[id=rubyDscrptn]:checked").val(); // else if question1 the value "ruby" is selected then
+          alert("esle if #1 - ruby");
+          $("input:radio[id=ruby]:checked").val(); // else if question1 the value "ruby" is selected then
           $("#result#rubyDscrptn").show(); // display #rubyDscrptn within #result section
         } // closes first else if statement
         else if (true) {
-          $("input:radio[id=cSharpDscrptn]:checked").val(); // else if question1 the value "cSharp" is selected then
+          alert("else if #2 - c#");
+          $("input:radio[id=cSharp]:checked").val(); // else if question1 the value "cSharp" is selected then
           $("#result#cSharpDscrptn").show(); // display #cSharpDscrptn within #result section
         } // closes second else if statement
       else {
+        alert("esle - php");
         $("#result#phpDscrptn").show(); // display #phpDscrption within #result section
       } // closes else statement
-    }); //closes submitbtn click
 
-    event.preventDefault();
+      event.preventDefault();
+
+    }); //closes submitbtn click
   }); //closes beginbtn click
 }); //closes document.ready

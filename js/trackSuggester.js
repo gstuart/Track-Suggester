8e5926e92682ddea9d86ae8794110e027ff50b66
Grace Stuart <gstuart@gmail.com>
2017-02-18 19:12:19 -0800

    change fadein/out to show/hide.

--- js/trackSuggester.js
+++ js/trackSuggester.js
@@ -2,15 +2,15 @@ $(document).ready(function() {
   //radioChoice = $("input:radio[id=radioBtn]:checked").val; // variable for all question selections, whether yes or no.
 
   $("button#beginBtn").click(function() { // when the button is clicked do the following:
-    $("button#beginBtn").fadeOut(); //this will hide the beginbtn
-    $(".jumbotron").fadeOut(); //this will hide the jubmotron
+    $("button#beginBtn").hide(); //this will hide the beginbtn
+    $(".jumbotron").hide(); //this will hide the jubmotron
     $("#questions").show(); //this will display the form.
-    $("button#submitBtn").fadeIn();
+    $("button#submitBtn").show();
     $("button#submitBtn").submit(function() {
       $("#questions").fadeOut(); // hide #questions
-
       // hide #submitBtn
       // function to determine which course user should take.
+        //
     }); //closes submitbtn click
 
     event.preventDefault();

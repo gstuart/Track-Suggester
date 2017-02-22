384a35924d9ad1ff94375b4bca164a1be8ba4380
Grace Stuart <gstuart@gmail.com>
2017-02-22 07:42:16 -0800

    remove quotes from result in the .show after if else statement.

--- js/scripts.js
+++ js/scripts.js
@@ -37,7 +37,7 @@ $(document).ready(function() {
         alert("Please select an answer for all questions.") // displays if none of the selections in question6 are made.
         $("#questions, button#submitBtn").show(); // display the form, panels, and submit button
       } // closes else statement
-      $("#result").show("result");
+      $("#result").show(result);
       event.preventDefault();
     }); //closes submitbtn click
   }); //closes beginbtn click

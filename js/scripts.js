89ac129d65047f350451c911470e5ef3ae06d989
Grace Stuart <gstuart@gmail.com>
2017-02-22 17:42:30 -0800

    removed comments.

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,33 @@
 $(document).ready(function() {
-  // user interface, or front-end, logic:
-  $("button#beginBtn").click(function() { // when the button is clicked, do the following:
-    $("button#beginBtn, #welcome").hide(); // hide the beginbtn and jubmotron
-    $("#questions, button#submitBtn").show(); // display the form, submit button, and panels
-    $("button#submitBtn").click(function() { //when submit button is clicked, do the following:
-      $("#questions, button#submitBtn").hide(); // hide #questions and submitbutton
-      // function to determine which course description to display.
-      // business, or back-end, logic:
-      // following are commented out until function using them is created.
-      // var q1Selection = $("input:radio[name=question1]:checked").val()
-      // var q2Selection = $("input:radio[name=question2]:checked").val();
-      // var q3Selection = $("input:radio[name=question3]:checked".val();
-      // var q4Selection = $("input:radio[name=question4]:checked".val();
-      // var q5Selection = $("input:radio[name=question5]:checked".val();
-      var q6Selection = $("input:radio[name=question6]:checked").val(); // variable for question6 selections
+  $("button#beginBtn").click(function() {
+    $("button#beginBtn, #welcome").hide();
+    $("#questions, button#submitBtn").show();
+    $("button#submitBtn").click(function() {
+      $("#questions, button#submitBtn").hide();
+      var q6Selection = $("input:radio[name=question6]:checked").val();
       var result;
-      if (q6Selection === "java") { // if the value of q6 is java, do the following:
+      if (q6Selection === "java") {
         result = javaDscrptn;
-        $("#result").show(result); // display #javaDscrption within #result section.
-      //  result.show();
-      } // closes first else if statement
+        $("#result").show(result); // displays all content within #result section.
+      //  result.show(); //when this is used the
+      }
         else if (q6Selection === "ruby") {
           alert("ruby result");
-          result = rubyDscrptn; // display #rubyDscrptn within #result section
-
-        } // closes second else if statement
+          result = rubyDscrptn;
+        }
         else if (q6Selection === "cSharp") {
           alert("ruby result");
-          result = cSharpDscrptn; // display #cSharpDscrptn within #result section
-        } // closes third else if statement
+          result = cSharpDscrptn;
+        }
         else if (q6Selection === "php") {
           alert("php result");
-          result = phpDscrptn; // display #phpDscrption within #result section
-        } // closes fourth else if statement
+          result = phpDscrptn;
+        }
       else {
-        alert("Please select an answer for all questions.") // displays if none of the selections in question6 are made.
-        $("#questions, button#submitBtn").show(); // display the form, panels, and submit button
-      } // closes else statement
-  //    $("#result").show(result);
+        alert("Please select an answer for all questions.")
+        $("#questions, button#submitBtn").show();
+      } 
       event.preventDefault();
-    }); //closes submitbtn click
-  }); //closes beginbtn click
+    });
+  });
 }); //closes document.ready

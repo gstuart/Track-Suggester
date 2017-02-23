f9dd60a41cd76ab19a6e9f7ff45e806be4f5c837
Grace Stuart <gstuart@gmail.com>
2017-02-22 17:53:46 -0800

    remove quotes from around #javaDscrptn.

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
       var q6Selection = $("input:radio[name=question6]:checked").val();
       var result;
       if (q6Selection === "java") {
-        result = ("#javaDscrptn");
+        result = (#javaDscrptn); 
         $("#result").show(result); // displays all content within #result section.
       //  result.show(); //when this is used the
       }

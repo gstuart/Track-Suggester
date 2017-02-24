2c43c6b8c97a711463eaa76f31a92bb6da3aa98a
Grace Stuart <gstuart@gmail.com>
2017-02-24 06:46:18 -0800

    seperate back and front end logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,30 @@
+// Back-end logic
+var q6Selection = $("input:radio[name=question6]:checked").val();
+
+if (q6Selection === "java") {
+  $("#javaDscrptn, #result").show();
+}
+  else if (q6Selection === "ruby") {
+    $("#result, #rubyDscrptn").show();
+  }
+  else if (q6Selection === "cSharp") {
+    $("#result, #cSharpDscrptn").show();
+  }
+  else if (q6Selection === "php") {
+    $("#result, #phpDscrptn").show();
+  }
+else {
+  alert("Please select an answer for all questions.")
+  $("#questions, button#submitBtn").show();
+}
+// Front-end logic below this line
 $(document).ready(function() {
   $("button#beginBtn").click(function() {
     $("button#beginBtn, #welcome").hide();
     $("#questions, button#submitBtn").show();
     $("button#submitBtn").click(function() {
       $("#questions, button#submitBtn").hide();
-      var q6Selection = $("input:radio[name=question6]:checked").val();
-      if (q6Selection === "java") {
-        $("#javaDscrptn, #result").show();
-      }
-        else if (q6Selection === "ruby") {
-          $("#result, #rubyDscrptn").show();
-        }
-        else if (q6Selection === "cSharp") {
-          $("#result, #cSharpDscrptn").show();
-        }
-        else if (q6Selection === "php") {
-          $("#result, #phpDscrptn").show();
-        }
-      else {
-        alert("Please select an answer for all questions.")
-        $("#questions, button#submitBtn").show();
-      }
+
       event.preventDefault();
     });
   });

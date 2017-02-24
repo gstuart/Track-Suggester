d54ccb9850ccce7f92e2557e12cb4e7c19cdcec7
Grace Stuart <gstuart@gmail.com>
2017-02-24 06:56:48 -0800

    add function on back end to run input, and called function in front end logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,35 @@
 // Back-end logic
-var q6Selection = $("input:radio[name=question6]:checked").val();
+var result = function(q6Selection){
+  if (q6Selection === "java") {
+    $("#javaDscrptn, #result").show();
+  }
+    else if (q6Selection === "ruby") {
+      $("#result, #rubyDscrptn").show();
+    }
+    else if (q6Selection === "cSharp") {
+      $("#result, #cSharpDscrptn").show();
+    }
+    else if (q6Selection === "php") {
+      $("#result, #phpDscrptn").show();
+    }
+  else {
+    alert("Please select an answer for all questions.")
+    $("#questions, button#submitBtn").show();
+  }
+};// need to return back-end logic
 
-if (q6Selection === "java") {
-  $("#javaDscrptn, #result").show();
-}
-  else if (q6Selection === "ruby") {
-    $("#result, #rubyDscrptn").show();
-  }
-  else if (q6Selection === "cSharp") {
-    $("#result, #cSharpDscrptn").show();
-  }
-  else if (q6Selection === "php") {
-    $("#result, #phpDscrptn").show();
-  }
-else {
-  alert("Please select an answer for all questions.")
-  $("#questions, button#submitBtn").show();
-}
 // Front-end logic below this line
 $(document).ready(function() {
   $("button#beginBtn").click(function() {
+    event.preventDefault();
     $("button#beginBtn, #welcome").hide();
     $("#questions, button#submitBtn").show();
     $("button#submitBtn").click(function() {
       $("#questions, button#submitBtn").hide();
+      var q6Selection = $("input:radio[name=question6]:checked").val();
+      var output = result(userInput);
+      // need to call back end logic
 
-      event.preventDefault();
     });
   });
 });

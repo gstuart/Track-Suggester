6ce982dc0997fad36da72073f036e32120626298
Grace Stuart <gstuart@gmail.com>
2017-02-23 17:52:02 -0800

    remove comment to perry.

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
       $("#questions, button#submitBtn").hide();
       var q6Selection = $("input:radio[name=question6]:checked").val();
       if (q6Selection === "java") {
-        $("#javaDscrptn, #result").show(); //this is were my error was. I was trying to combine them in a different way I found in one of my googling another bug but that didn't work. Lesson learned: stick to what I know from the lesson until I have more experience and hopefully I can save myself 5 hours of debugging time. I would still like to know how to make the prior code work, if it is possible.
+        $("#javaDscrptn, #result").show();
       }
         else if (q6Selection === "ruby") {
           $("#result, #rubyDscrptn").show();

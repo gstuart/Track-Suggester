19a7ce8715a874edede38228f7223a719ca4a51e
Grace Stuart <gstuart@gmail.com>
2017-02-28 20:19:50 -0800

    remove submit.click function from beginBtn.click function.

--- js/scripts.js
+++ js/scripts.js
@@ -2,26 +2,26 @@ $(document).ready(function() {
   $("button#beginBtn").click(function() {
     $("button#beginBtn, #welcome").hide();
     $("#questions, button#submitBtn").show();
-    $("button#submitBtn").click(function() {
-      $("#questions, button#submitBtn").hide();
-      var q6Selection = $("input:radio[name=question6]:checked").val();
-      if (q6Selection === "java") {
-        $("#javaDscrptn, #result").show();
+  });
+  $("button#submitBtn").click(function() {
+    $("#questions, button#submitBtn").hide();
+    var q6Selection = $("input:radio[name=question6]:checked").val();
+    if (q6Selection === "java") {
+      $("#javaDscrptn, #result").show();
+    }
+      else if (q6Selection === "ruby") {
+        $("#result, #rubyDscrptn").show();
       }
-        else if (q6Selection === "ruby") {
-          $("#result, #rubyDscrptn").show();
-        }
-        else if (q6Selection === "cSharp") {
-          $("#result, #cSharpDscrptn").show();
-        }
-        else if (q6Selection === "php") {
-          $("#result, #phpDscrptn").show();
-        }
-      else {
-        alert("Please select an answer for all questions.")
-        $("#questions, button#submitBtn").show();
+      else if (q6Selection === "cSharp") {
+        $("#result, #cSharpDscrptn").show();
       }
-      event.preventDefault();
-    });
+      else if (q6Selection === "php") {
+        $("#result, #phpDscrptn").show();
+      }
+    else {
+      alert("Please select an answer for all questions.")
+      $("#questions, button#submitBtn").show();
+    }
+    event.preventDefault();
   });
 });
